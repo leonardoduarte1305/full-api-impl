b9858c6857012ee9709c76ca500086a1fbd72455
Leonardo Goncalves Duarte da Silva (CodeBrain) <codebrain.leonardo.dasilva@kiwigrid.com>
2023-02-23 20:36:49 -0300

    Created the project - Api Specification is already working fine - mvn spring-boot:run works fine from the service module

---
+++ service/src/test/java/dev/leoduarte/fullapiimpl/FullApiImplApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leoduarte.fullapiimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullApiImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
